b2bb826a2f6321b0506b668ea5f730519588d45d
Kumar9113 <12006015@svuceedu.onmicrosoft.com>
2023-11-07 08:57:49 +0530

    change

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-const express=require('express')
-const app=express();
+const express = require('express')
+const app = express();
 app.use(express.json());
 
-const movieRoute=require("./routers/movies")
-const authRoute=require("./routers/auth")
-const userRouter=require("./routers/users")
+const movieRoute = require("./routers/movies")
+const authRoute = require("./routers/auth")
+const userRouter = require("./routers/users")
 const listRoute = require("./routers/list");
 
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
 const { MongoClient, ServerApiVersion } = require('mongodb');
-mongoose.connect('mongodb+srv://Kumar:Kumar%409113@cluster0.bprqnop.mongodb.net/?retryWrites=true&w=majority',
-{useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1} 
-).then(()=>
-{
+const configOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+};
+const connectionUrl = "mongodb+srv://Kumar:Kumar%409113@cluster0.xrhjxpn.mongodb.net/?retryWrites=true&w=majority";
+mongoose.connect(connectionUrl, configOptions).then(() => {
     console.log('db connecct')
-}).catch(err=>console.log(err))
+}).catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+    res.json(
+        {
+            status: 200,
+            message: "welcome to page"
+
+        }
+    )
+})
 
 
 
-app.use("/api/auth",authRoute)
-app.use("/api/users",userRouter);
-app.use("/api/movies",movieRoute);
+app.use("/api/auth", authRoute)
+app.use("/api/users", userRouter);
+app.use("/api/movies", movieRoute);
 app.use("/api/lists", listRoute);
 
-app.listen(3000,()=>
-{
+app.listen(3000, () => {
     console.log("Server start");
 })
 
